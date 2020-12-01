bb2f76764f31441b3a9ae25d931da2e20a3d6518
Patrick Ford <patrickfordp@gmail.com>
2020-11-30 22:14:20 -0800

    Commit game

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] 
+    private Text textComponent;
+
+    [SerializeField]
+    private State startingState;
+
+    private State _state;
+
+    private State state
+    {
+        get => _state;
+        set
+        {
+            _state = value;
+            textComponent.text = _state.GetStateStory();
+        }
+    }
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        state = startingState;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for( int index = 0; index < nextStates.Length; index++)
+        {
+            if( Input.GetKeyDown( KeyCode.Alpha1 + index ) )
+            {
+                state = nextStates[index];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = nameof(State))]
+public class State : ScriptableObject
+{
+    [TextArea(15,20)]
+    [SerializeField] 
+    private string storyText;
+
+    [SerializeField]
+    private State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
